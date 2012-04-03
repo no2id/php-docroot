8e0149123b3272faec09fa11aceb0fe31f612b2d
adam mcgreggor <adam@no2id.net>
2012-04-03 22:43:46 +0100

    get the right phone number for Guy

--- news/index.php
+++ news/index.php
@@ -19,7 +19,7 @@ require_once("../inc/pageHead.php");?>
 <p>For up-to-date information, or immediate or future interview, please contact:</p>
 <ul>
     <li>James Baker, <a href="mailto:campaigns@no2id.net">Campaigns Manager</a> &#8211; 078 1760 5162</li>
-    <li>Guy Herbert, <a href="mailto:general.secretary@no2id.net">General Secretary</a> &#8211; 079 7423 0839</li>
+    <li>Guy Herbert, <a href="mailto:general.secretary@no2id.net">General Secretary</a> &#8211; 079 5654 4308</li>
 </ul>
 
 <p>General media enquiries can be sent to <a href="mailto:press&#x40;no2id.net">press&#x40;no2id.net</a></p>
