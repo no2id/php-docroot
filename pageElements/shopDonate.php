ff1f73ad6926cb1e1612d55f1f0060414f07ade4
adam mcgreggor <adam@no2id.net>
2012-04-03 23:05:11 +0100

    fix-up path error

--- pageElements/shopDonate.php
+++ pageElements/shopDonate.php
@@ -2,7 +2,7 @@
 	<h1>Donate</h1>
         <div style="text-align: center; padding: 0.2em;">
 		<p style="text-align:center"><a href="/get-involved/donate">Donate online or by credit card</a></p>
-        <? include('get-involved/ppButton.php'); ?>
+        <? include('../get-involved/ppButton.php'); ?>
         </div>
 <p style="text-align:center"><a href="/get-involved/shop">or buy merchandise<br /><img src="/images/sticker_ad.png" alt="Get a NO2ID window sticker" width="121" height="118" /></a></p>
 </div>
