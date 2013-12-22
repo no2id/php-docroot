ecd99d696d7554efd9d1f5a92933015441e24194
adam mcgreggor <adam@no2id.net>
2013-12-22 23:53:58 +0000

    Make static, fix-up links

--- about/supporters.php
+++ about/supporters.php
@@ -1,54 +1,135 @@
 <?php
 $page_title = "our supporters";
 require_once("../inc/pageHead.php");
-require_once("../inc/dbConn.php");
 ?>
+
 <h1>Our Supporters</h1>
 <p>NO2ID is supported by a broad range of organisations, all of whom are concerned about the proposed imposition
 of national identity cards onto the UK population.</p>
 <p>Should your organisation be on this list? If so please <a href="mailto:affiliates&#x40;no2id.net">get 
   in touch</a>.</p> 
 <div style="clear:both">
-<?php
-// create field of supporter logos
-$i_list_sql = "SELECT name,logo FROM supporter_org WHERE logo <> ''";
-$i_list = mysql_query($i_list_sql) or die(mysql_error());
-while ($org = mysql_fetch_assoc($i_list)){
 
-	echo "<div style=\"height:91px;float:left;margin:5px;\"><img src=\"/images/affiliateLogos/{$org['logo']}\" alt=\"{$org['name']} support NO2ID\"/></div>\n";
-}
-?>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/1990.gif" alt="1990 Trust support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/ARCH.gif" alt="Action on Rights for Children (ARCh) support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/activistNetwork.gif" alt="Activist Network support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/charter88.gif" alt="Charter 88 support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/freedomassoc.gif" alt="Freedom Association support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/fipr.gif" alt="Foundation for Information Policy Research support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/greenparty.gif" alt="Green Party of England and Wales support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/globRes.gif" alt="Globalise Resistance support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/index.gif" alt="Index on Censorship support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/libdem.gif" alt="Liberal Democrats support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/liberal.gif" alt="Liberal Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/liberty.gif" alt="Liberty support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/NEPD.gif" alt="Network for Economic &amp; Political Democracy support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/newAlliance.gif" alt="New Alliance support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/pi.gif" alt="Privacy International support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/radicalActivistNet.gif" alt="Radical Activist Network support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/scotHumRights.gif" alt="Scottish Human Rights Centre support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/SEA.gif" alt="Socialist Environmental Alliance support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/stand.gif" alt="Stand.org.uk support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/statewatch.gif" alt="Statewatch support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/slplogo.gif" alt="Socialist Labour Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/respect.gif" alt="RESPECT - The Unity Coalition support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/ukip.gif" alt="UK Independence Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/reformist.gif" alt="Reformist Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/unityinjustice.gif" alt="Unity Injustice support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/norwichcouncil.gif" alt="Norwich City Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/yorkcouncil.gif" alt="York City Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/scotGreen.gif" alt="Scottish Green Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/cambridgeCityCouncil.gif" alt="Cambridge City Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/assocBritDrivers.gif" alt="Association of British Drivers support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/cib.gif" alt="Campaign for an Independent Britain support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/leicester.gif" alt="Leicester City Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/watford.gif" alt="Watford Borough Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/ceredigion.gif" alt="Ceredigion County Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/communityAction.gif" alt="Community Action Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/newhaven.gif" alt="Newhaven Town Council support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/SNP.gif" alt="Scottish Nationalist Party support NO2ID"/></div>
+<div style="height:91px;float:left;margin:5px;"><img src="/images/affiliateLogos/english_democrats.jpg" alt="English Democrats support NO2ID"/></div>
 </div>
+
 <div style="clear:both">
-<?php
-$support_groups = array('party' => 'Political Parties','ngo' => 'NGOs','tu' => 'Trades Unions','other' => 'Other');
+    <div class="supportersList"> 
+        <ul>
+            <li><a href="http://www.blink.org.uk/">1990 Trust</a></li>
+            <li><a href="http://www.arch-ed.org/">Action on Rights for Children (ARCh)</a></li>
+            <li><a href="http://www.activistnetwork.org.uk/">Activist Network</a></li>
+            <li><a href="http://www.alternativeparty.org.uk/">Alternative Party</a></li>
+            <li><a href="http://www.abd.org.uk/">Association of British Drivers</a></li>
+            <li><a href="http://www.cambridge.gov.uk/">Cambridge City Council</a></li>
+            <li><a href="http://www.cacc.org.uk/">Campaign Against Criminalising Communities</a></li>
+            <li><a href="http://www.cibhq.co.uk/">Campaign for an Independent Britain</a></li>
+            <li><a href="http://www.cardiffsocialforum.org.uk/">Cardiff Social Forum</a></li>
+            <li><a href="http://www.ceredigion.gov.uk/">Ceredigion County Council</a></li>
+            <li><a href="http://www.charter88.org.uk/">Charter 88</a></li>
+            <li><a href="http://www.theplace4.co.uk/elvisseeninbaghdad/">Church of the Militant Elvis Party</a></li>
+            <li><a href="http://www.community-action.com/">Community Action Party</a></li>
+            <li><a href="http://www.freewales.info/">Cymru Rydd - The Welsh Republican Party</a></li>
+            <li><a href="http://www.democracymovement.org.uk/">Democracy Movement</a></li>
+            <li>Democratic Labour Party</li>
+            <li><a href="http://www.englishdemocrats.org.uk/">English Democrat Party</a></li>
+            <li><a href="http://www.englishdemocrats.org.uk/">English Democrats</a></li>
+            <li><a href="http://www.fipr.org/">Foundation for Information Policy Research</a></li>
+            <li><a href="http://www.tfa.net/">Freedom Association</a></li>
+            <li><a href="http://www.resist.org.uk/">Globalise Resistance</a></li>
+            <li><a href="http://www.leeds.ac.uk/union/socs/greenaction/">Green Action</a></li>
+            <li><a href="http://www.greenparty.org.uk/">Green Party of England and Wales</a></li>
+            <li><a href="http://www.hackney-labour.org.uk/">Hackney Labour Party</a></li>
+            <li>Hackney Unison</li>
+            <li>Halton Citizens Party</li>
+            <li>Hebden Royd Town Council</li>
+            <li>Independent Group of Councillors (Kingston)</li>
+            <li><a href="http://www.indexonline.org/">Index on Censorship</a></li>
+            <li><a href="http://www.lca-uk.org/">Legalise Cannabis Alliance</a></li>
+            <li><a href="http://www.leicester.gov.uk/">Leicester City Council</a></li>
+            <li><a href="http://www.libdems.org.uk/">Liberal Democrats</a></li>
+            <li><a href="http://www.liberal.org.uk/">Liberal Party</a></li>
+            <li><a href="http://www.libertarian.co.uk/">Libertarian Alliance</a></li>
+            <li><a href="http://www.liberty-human-rights.org.uk/">Liberty</a></li>
+            <li><a href="http://www.nepd.org.uk/idcard.htm">Network for Economic &amp; Political Democracy</a></li>
+            <li><a href="http://www.newalliance.org.uk/idcards.htm">New Alliance</a></li>
+            <li>Newhaven Town Council</li>
+            <li><a href="http://www.norwich.gov.uk/">Norwich City Council</a></li>
+            <li><a href="http://www.peaceparty.org.uk/">Peace Party</a></li>
+            <li><a href="http://www.plaidcymru.org/">Plaid Cymru</a></li>
+            <li><a href="http://www.privacyinternational.org/">Privacy International</a></li>
+            <li><a href="http://www.q-news.com/">Q-News</a></li>
+            <li><a href="http://www.radicalactivist.net/">Radical Activist Network</a></li>
+            <li>Reformist Party</li>
+            <li>Resolutionist Party</li>
+            <li><a href="http://www.respectcoalition.org/">RESPECT - The Unity Coalition</a></li>
+            <li><a href="http://www.scottishgreens.org.uk/">Scottish Green Party</a></li>
+            <li><a href="http://www.scottishhumanrightscentre.org.uk/">Scottish Human Rights Centre</a></li>
+            <li><a href="http://www.scottishindependenceparty.org/">Scottish Independence Party</a></li>
+            <li><a href="http://www.snp.org/">Scottish Nationalist Party</a></li>
+            <li><a href="http://www.scottishsocialistparty.org/">Scottish Socialist Party</a></li>
+            <li>Silent Majority Party</li>
+            <li><a href="http://www.ethnicsociety.com/">Social Vision</a></li>
+            <li><a href="http://socialistenvironmentalalliance.org/">Socialist Environmental Alliance</a></li>
+            <li><a href="http://www.socialist-labour-party.org.uk/">Socialist Labour Party</a></li>
+            <li>Socialist People's Party</li>
+            <li><a href="http://www.sgp.saltire.org/">Southwark Green Party</a></li>
+            <li>St Albans Party</li>
+            <li><a href="http://www.stand.org.uk/">Stand.org.uk</a></li>
+            <li><a href="http://www.statewatch.org/">Statewatch</a></li>
+            <li><a href="http://www.ukip.org/">UK Independence Party</a></li>
+            <li><a href="http://www.westbournemouthukip.com/">UKIP Bournemouth West</a></li>
+            <li><a href="http://www.unison.org.uk/">UNISON</a></li>
+            <li>UNISON City of Plymouth</li>
+            <li><a href="http://www.unison.org.uk/southwest/">UNISON South West</a></li>
+            <li>United Socialist Party</li>
+            <li><a href="http://unity-injustice.5u.com/">Unity Injustice</a></li>
+            <li><a href="http://www.watford.gov.uk/">Watford Borough Council</a></li>
+            <li>West Suffolk Independent Alliance Party</li>
+            <li><a href="http://www.york.gov.uk/">York City Council</a></li>
+        </ul>
 
-//foreach ($support_groups as $type => $label) {
-//$s_list_sql = "SELECT name,homepage FROM supporter_org WHERE type = '$type' ORDER BY name";
-
-$s_list_sql = "SELECT name,homepage FROM supporter_org ORDER BY name";
-$s_list = mysql_query($s_list_sql) or die(mysql_error());
-echo '<div class="supportersList">';
-//echo "<h2>$label</h2>";
-?> 
-<ul>
-  <?php
-		  while ($org = mysql_fetch_assoc($s_list)){
-		  	echo '<li>';
-			if ($org['homepage'] != ''){
-				echo "<a href=\"{$org['homepage']}\">";
-			}
-			echo "{$org['name']}";
-			if ($org['homepage'] != ''){
-				echo '</a>';
-			}
-			echo '</li>';
-		  }
-			echo '</ul>';
-			echo '</div>';
-//}
-	  ?>
 </div>
+</div>
+
 <p style="clear:both;">Plus a growing number of individuals and <a href="mp_supporters.php">several 
 	Members of Parliament</a></p>
+
 <?php require_once("../inc/pageFooter.php");?>
