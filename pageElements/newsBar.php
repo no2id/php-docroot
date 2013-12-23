3f7faf268a5ced5a18a23d9deacdd69fbae14f62
Adam McGreggor <adam@amyl.org.uk>
2013-12-23 01:27:03 +0000

    <p> to styleize

--- pageElements/newsBar.php
+++ pageElements/newsBar.php
@@ -24,7 +24,7 @@
 
 <div id="twitters" class="sideBar">
     <h1>Twitter</h1>
-    <a href="https://twitter.com/no2id" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @no2id</a>
+    <p><a href="https://twitter.com/no2id" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @no2id</a></p>
     <script>!function(d,s,id){var
     js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,
     'script', 'twitter-wjs');</script>
