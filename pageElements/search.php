88f4e0ee43eea12c45aba38e21e6d1ee415144cb
adam mcgreggor <adam@adamamyl.com>
2015-12-21 20:37:52 +0000

    Search on Google via SSL

--- pageElements/search.php
+++ pageElements/search.php
@@ -1,9 +1,9 @@
 <div id="googleSearchBlock">
-	<form method="get" action="http://www.google.com/u/NO2ID">
+	<form method="get" action="https://www.google.com/u/NO2ID">
 		<div>
 		<label for="q">Search: </label>
 		<input type="text" name="q" id="q" maxlength="255" size="12"/>
 		</div>
-		<p style="clear:both; font-size:smaller;">Search provided by <a href="http://www.google.com/">Google</a></p>
+		<p style="clear:both; font-size:smaller;">Search provided by <a href="https://www.google.com/">Google</a></p>
 	</form>
 </div>
